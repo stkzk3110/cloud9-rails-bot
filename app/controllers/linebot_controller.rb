3e18ec6724b708c3b7e639fa3a1fb4b7748bac72
stkzk3110 <fisherman0825dd@gmail.com>
2019-06-10 17:35:19 +0900

    add Mcdonald

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -19,28 +19,45 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::Message
           case event.type
           when Line::Bot::Event::MessageType::Text
-            message = {
-              type: 'text',
-              text: event.message['text']
-            }
-            client.reply_message(event['replyToken'], message)
+            msg = event.message['text']
+            if send_msg(msg)
+              logger.debug('callback true')
+              logger.debug(msg)
+              msg = "I'm lovin it!"
+              logger.debug(msg)
+              client.reply_message(event['replyToken'], {
+                type: 'text',
+                text: msg});
+            else
+              logger.debug('callback false')
+              client.reply_message(event['replyToken'], {
+                type: 'text',
+                text: msg});
+            end
           end
         end
       }
       head :ok
     end
 
+    def send_msg(msg)
+      logger.debug(msg)
+      logger.debug('send_msg')
+      if msg == 'マクドナルド'
+        logger.debug('send_msg true')
+        return true
+      else
+        logger.debug('send_msg false')
+        false
+      end
+    end
+
     private
 
     def client
       @client ||= Line::Bot::Client.new { |config|
-        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+        config.channel_secret = "36674aa7a7beb7289c5d40ebef4829d1"
+        config.channel_token = "NXGApTHf8F78/pYtc5FdeE7ci3w5ajyXdAYng3gYVUvZE/7KI/Ib9kelOLwMvMratkAosyqv/zNQfHxI+NtenaFhQuBHGba/i29eOxW1I8V1uu1+ySsFlY160utrDWzJfRm2fZg9sqmouled5/8jWQdB04t89/1O/w1cDnyilFU="
       }
     end
-    # def send_msg(msg)
-    #   if msg.text == 'マクドナルド'
-    #     client.reply_message(event['replyToken'], `I'm lovin it!`)
-    #   end
-    # end
   end
